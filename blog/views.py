f4e3f1aeae36fcb12a4bdf7cadf9922ba6dc8bca
Ellenor Vondrus <ellavondrus@gmail.com>
2024-04-17 17:12:28 +0200

    Update to generic Post view
     Change url paths and add post_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-from django.http import HttpResponse
 # Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
 
-
-def my_blog(request):
-    return HttpResponse("Hello, world. You're at my blog.")

07bb5c65ccf3c901bdfcd578be1431ac131b9801
Ellenor Vondrus <ellavondrus@gmail.com>
2024-04-19 17:41:57 +0200

    Add html, css and inherit templates
    Add base template and css styles to top-level directory and inherit to index.html in blog app
    
     Connect the directories in settings.py and create the view in blog app

--- codestar/settings.py
+++ codestar/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 if os.path.exists('env.py'):
     import env
@@ -62,7 +65,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -134,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
